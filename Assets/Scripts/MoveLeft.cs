2c14ad2414a1fc1ccfcfff2f65ed358be1e5e2d6
My_Dude <23wingj@live.elida.k12.oh.us>
2022-09-06 12:27:36 -0400

    Made player follow world, Started animations

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
 
-    public float speed = 30.0f;
+    public float speed = 10.0f;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        //Moves Background and Obstacles Left
+        if (playerControllerScript.gameOver == false)
+        {
+
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        }
+        //Destroys Obstacles if past x range
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+
+            Destroy(gameObject);
+
+        }
     }
 }
